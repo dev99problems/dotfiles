3c0888fe60430b8079c9e73989ddbf47430f9a57
Eugene Chulkov <genechulkov@gmail.com>
2022-10-03 10:21:15 +0100

    [nvim] introduce packer and plugins.lua

---
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -0,0 +1,139 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  print "Packer can not be loaded!"
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require('packer.util').float { border = 'rounded' }
+    end,
+  },
+}
+
+return packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-lua/popup.nvim'
+
+  -- Telescope
+  use {
+    'nvim-telescope/telescope.nvim', 
+    tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use { 'nvim-telescope/telescope-file-browser.nvim' }
+
+  -- lsp
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use {
+    'tzachar/cmp-tabnine', 
+    run='./install.sh', 
+    requires = 'hrsh7th/nvim-cmp'
+  }
+
+  use {
+    'windwp/nvim-autopairs',
+    config = function() require('nvim-autopairs').setup {} end
+  }
+  use 'airblade/vim-gitgutter'
+  use 'norcalli/nvim-colorizer.lua'
+
+  -- fzf
+  use { 'junegunn/fzf', run = ':call fzf#install()' }
+  use { 'junegunn/fzf.vim' }
+
+  -- Treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use 'nvim-treesitter/nvim-treesitter-context'
+
+  -- Power lines
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use {
+    'akinsho/bufferline.nvim', 
+    tag = 'v2.*', 
+    requires = 'kyazdani42/nvim-web-devicons'
+  }
+
+  -- Icons
+  use 'ryanoasis/vim-devicons'
+  use 'kyazdani42/nvim-web-devicons' 
+
+  -- File Trees
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons'
+    }
+  }
+
+  -- Plug 'ThePrimeagen/vim-be-good'
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-surround'
+
+  -- Scroll
+  use 'karb94/neoscroll.nvim'
+
+  -- Trouble
+  use {
+    'folke/trouble.nvim',
+    requires = 'kyazdani42/nvim-web-devicons'
+  }
+
+  -- Themes
+  use 'gruvbox-community/gruvbox'
+  use {
+    'dracula/vim', 
+    as = 'dracula' 
+  }
+  use 'drewtempelmeyer/palenight.vim'
+  use { 
+    'sonph/onehalf', 
+    rtp = 'vim' 
+  }
+
+
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
+
