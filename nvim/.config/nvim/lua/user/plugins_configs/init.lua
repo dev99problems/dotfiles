81197febf9ccc33d5b6dc4fc82a5b8b347db03f2
Eugene Chulkov <genechulkov@gmail.com>
2022-10-06 23:16:21 +0100

    [nvim] rename plugins related stuff to a more understandable

---
+++ nvim/.config/nvim/lua/user/plugins_configs/init.lua
@@ -0,0 +1,6 @@
+require('user.plugins_configs.telescope')
+require('user.plugins_configs.bufferline')
+require('user.plugins_configs.misc')
+-- winbar requires NeoVim nightly, aka 0.9.x
+-- require('user.plugins.winbar')
+
