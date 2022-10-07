6f2e442a3cc41c0bbb7cf144ee934af1ddee5342
Eugene Chulkov <genechulkov@gmail.com>
2022-10-07 14:20:25 +0100

    [nvim] additional lsp config

--- nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -1,33 +1,42 @@
 local utils = require('user.utils')
 local lsp_installer = utils.safe_require('nvim-lsp-installer')
+local lspconfig = utils.safe_require('lspconfig')
+local lsp_handlers = require('user.lsp.handlers')
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead.
-local tbl_extend = vim.tbl_deep_extend
+lsp_installer.setup({
+  ui = {
+    icons = {
+       server_installed = "✓",
+       server_pending = "➜",
+       server_uninstalled = "✗"
+    }
+  }
+})
 
-lsp_installer.on_server_ready(function(server)
-	local opts = {
-		on_attach = require('user.lsp.handlers').on_attach,
-		capabilities = require('user.lsp.handlers').capabilities,
-	}
+local opts = {
+  on_attach = lsp_handlers.on_attach,
+  capabilities = lsp_handlers.capabilities,
+}
 
-	 if server.name == 'jsonls' then
-	 	local jsonls_opts = require('user.lsp.settings.jsonls')
-	 	opts = tbl_extend('force', jsonls_opts, opts)
-	 end
+local sumneko_settings = require('user.lsp.settings.sumneko_lua').settings
+lspconfig.sumneko_lua.setup {
+  attach = opts.on_attach,
+  capabilities = opts.capabilities,
+  settings = sumneko_settings
+}
 
-	 if server.name == 'sumneko_lua' then
-	 	local sumneko_opts = require('user.lsp.settings.sumneko_lua')
-	 	opts = tbl_extend('force', sumneko_opts, opts)
-	 end
+local pyright_settings = require('user.lsp.settings.pyright').settings
+lspconfig.pyright.setup {
+  attach = opts.on_attach,
+  capabilities = opts.capabilities,
+  settings = pyright_settings
+}
 
-	 if server.name == 'pyright' then
-	 	local pyright_opts = require('user.lsp.settings.pyright')
-	 	opts = tbl_extend('force', pyright_opts, opts)
-	 end
+local jsonls_settings = require('user.lsp.settings.jsonls').settings
+lspconfig.jsonls.setup {
+  attach = opts.on_attach,
+  capabilities = opts.capabilities,
+  settings = jsonls_settings
+}
 
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
-end)
 
