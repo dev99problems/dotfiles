6bb66db56d05ad579f14ab9f11ae5264fae76a73
Eugene Chulkov <genechulkov@gmail.com>
2022-10-09 18:39:02 +0100

    [nvim] add extended Telescope config with basic goodies

--- nvim/.config/nvim/lua/user/plugins_configs/telescope.lua
+++ nvim/.config/nvim/lua/user/plugins_configs/telescope.lua
@@ -1,34 +1,97 @@
 local utils = require('user.utils')
 local telescope = utils.safe_require('telescope')
--- local telescope = require('telescope')
--- local actions = require('telescope.actions')
+local actions = utils.safe_require('telescope.actions')
 
--- print('Ola from user.telescope config ')
-
-telescope.setup({
+telescope.setup {
   defaults = {
-    -- mappings = {
-    --   i = {
-    --     ["<C-s>"] = actions.cycle_previewers_next,
-    --     ["<C-a>"] = actions.cycle_previewers_prev,
-    --   },
-    -- },
-  }
-})
 
--- require('telescope').setup({
---   defaults = {
---     layout_config = {
---       vertical = { width = 0.2 }
---       -- other layout configuration here
---     },
---     -- other defaults configuration here
---   },
---   -- pickers = {
---   --   find_files = {
---   --     theme = "dropdown",
---   --   }
---   -- },
---   -- other configuration values here
--- })
+    prompt_prefix = ' ',
+    selection_caret = ' ',
+    -- path_display = { 'smart' },
+    layout_config = {
+      horizontal = { width = 0.9 }
+    },
 
+    mappings = {
+      i = {
+        ['<C-n>'] = actions.cycle_history_next,
+        ['<C-p>'] = actions.cycle_history_prev,
+
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous,
+
+        ['<C-c>'] = actions.close,
+
+        ['<Down>'] = actions.move_selection_next,
+        ['<Up>'] = actions.move_selection_previous,
+
+        ['<CR>'] = actions.select_default,
+        ['<C-x>'] = actions.select_horizontal,
+        ['<C-v>'] = actions.select_vertical,
+        ['<C-t>'] = actions.select_tab,
+
+        ['<C-u>'] = actions.preview_scrolling_up,
+        ['<C-d>'] = actions.preview_scrolling_down,
+
+        ['<PageUp>'] = actions.results_scrolling_up,
+        ['<PageDown>'] = actions.results_scrolling_down,
+
+        ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
+        ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
+        ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
+        ['<S-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+        ['<C-l>'] = actions.complete_tag,
+        ['<C-_>'] = actions.which_key, -- keys from pressing <C-/>
+        -- ['<D-e>'] = actions.oldfiles, -- keys from pressing <C-/>
+      },
+
+      n = {
+        ['<esc>'] = actions.close,
+        ['<CR>'] = actions.select_default,
+        ['<C-x>'] = actions.select_horizontal,
+        ['<C-v>'] = actions.select_vertical,
+        ['<C-t>'] = actions.select_tab,
+
+        ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
+        ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
+        ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
+        ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+
+        ['j'] = actions.move_selection_next,
+        ['k'] = actions.move_selection_previous,
+        ['H'] = actions.move_to_top,
+        ['M'] = actions.move_to_middle,
+        ['L'] = actions.move_to_bottom,
+
+        ['<Down>'] = actions.move_selection_next,
+        ['<Up>'] = actions.move_selection_previous,
+        ['gg'] = actions.move_to_top,
+        ['G'] = actions.move_to_bottom,
+
+        ['<C-u>'] = actions.preview_scrolling_up,
+        ['<C-d>'] = actions.preview_scrolling_down,
+
+        ['<PageUp>'] = actions.results_scrolling_up,
+        ['<PageDown>'] = actions.results_scrolling_down,
+
+        ['?'] = actions.which_key,
+      },
+    },
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  },
+}
