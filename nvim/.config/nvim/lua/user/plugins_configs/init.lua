373bfaf9ff1aa7cc54ad083083e225e0663f96fe
Eugene Chulkov <genechulkov@gmail.com>
2022-10-14 12:27:38 +0100

    [nvim] move out lualine to a separate config

--- nvim/.config/nvim/lua/user/plugins_configs/init.lua
+++ nvim/.config/nvim/lua/user/plugins_configs/init.lua
@@ -4,6 +4,7 @@ require('user.plugins_configs.misc')
 require('user.plugins_configs.treesitter')
 require('user.plugins_configs.neoscroll')
 require('user.plugins_configs.nvim-tree')
+require('user.plugins_configs.lualine')
 -- winbar requires NeoVim nightly, aka 0.9.x
 -- require('user.plugins.winbar')
 
