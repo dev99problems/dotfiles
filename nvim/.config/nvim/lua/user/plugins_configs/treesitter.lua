3cf19939ab5f84553cd75b1c7284e2f0a1703ef6
Eugene Chulkov <your_email>
2022-11-20 23:30:31 +0000

    [nvim] add blinking cursor; refine treesitter lang. list

--- nvim/.config/nvim/lua/user/plugins_configs/treesitter.lua
+++ nvim/.config/nvim/lua/user/plugins_configs/treesitter.lua
@@ -2,8 +2,8 @@ local utils = require('user.utils')
 local configs = utils.safe_require('nvim-treesitter.configs')
 
 configs.setup {
-  -- NOTE: maybe I should refine the num of configs later
-  ensure_installed = 'all',
+  ensure_installed = { 'html', 'javascript', 'typescript', 'tsx', 'css', 'json', 'ruby', 'python', 'go', 'sql' },
+  -- ensure_installed = 'all',
   sync_install = false,
   ignore_install = { '' }, -- List of parsers to ignore installing
   highlight = {
