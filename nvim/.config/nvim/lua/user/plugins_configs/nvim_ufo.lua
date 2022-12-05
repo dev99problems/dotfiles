8c4241b51c66239fb921282cebe1e7fd229f0782
Eugene Chulkov <genechulkov@gmail.com>
2022-12-05 23:11:33 +0000

    [nvim] add cheatsheet for folding keymaps

--- nvim/.config/nvim/lua/user/plugins_configs/nvim_ufo.lua
+++ nvim/.config/nvim/lua/user/plugins_configs/nvim_ufo.lua
@@ -10,3 +10,18 @@ ufo.setup({
 -- NOTE: if this set to e.g. 0, on opening file
 -- all the scopes will be folded by default
 vim.o.foldlevelstart = 99
+
+-- ## Basics
+-- zd — delete one fold at the cursor
+-- zD — delete all folds under cursor recursively
+-- zE — eliminate all folds in the window
+-- zo — open one fold
+
+-- za — fond/unfold a paragraph under cursor
+-- zA — fond/unfold a paragraph under cursor recursively 🤯
+
+-- ## Advanced
+-- Using `kevinhwang91/nvim-ufo` plugin for folding/unfolding there are 2 additional hotkeys:
+-- zM - fold everything
+-- zR - unfold everything
+
