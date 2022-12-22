e975d8980de980691c70fd9c9127e2a142e2198c
Eugene Chulkov <genechulkov@gmail.com>
2022-12-22 21:52:41 +0000

    [nvim] bigger float term 0.95x0.95

--- nvim/.config/nvim/lua/user/plugins_configs/floaterm.lua
+++ nvim/.config/nvim/lua/user/plugins_configs/floaterm.lua
@@ -2,6 +2,6 @@ local utils = require('user.utils')
 
 utils.safe_require('vim-floaterm').setup()
 
-vim.cmd "let g:floaterm_width=0.85"
-vim.cmd "let g:floaterm_height=0.9"
+vim.cmd "let g:floaterm_width=0.95"
+vim.cmd "let g:floaterm_height=0.95"
 vim.cmd "let g:floaterm_wintype='float'"
