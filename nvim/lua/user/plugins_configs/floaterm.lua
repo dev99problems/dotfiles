f4e81651a6902f66dce71465efe5a447fe8ad8a8
Gene Chulkov <genechulkov@gmail.com>
2023-05-14 20:44:22 +0100

    [nvim] remove floaterm setup, which is not needed anymore

--- nvim/lua/user/plugins_configs/floaterm.lua
+++ nvim/lua/user/plugins_configs/floaterm.lua
@@ -1,7 +1,3 @@
-local utils = require('user.utils')
-
-utils.safe_require('vim-floaterm').setup()
-
 vim.cmd "let g:floaterm_width=0.95"
 vim.cmd "let g:floaterm_height=0.95"
 vim.cmd "let g:floaterm_wintype='float'"
