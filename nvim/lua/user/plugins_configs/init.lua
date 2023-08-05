2ee2c94173da50fa56f3b48e49e3412e83d27f65
Gene Chulkov <genechulkov@gmail.com>
2023-08-05 16:13:21 +0100

    [nvim] remove neodim plugin

--- nvim/lua/user/plugins_configs/init.lua
+++ nvim/lua/user/plugins_configs/init.lua
@@ -14,8 +14,6 @@ require('user.plugins_configs.startify')
 require('user.plugins_configs.gitsigns')
 require('user.plugins_configs.minimap')
 require('user.plugins_configs.lsp_saga')
-require('user.plugins_configs.neodim')
 -- require('user.plugins_configs.solar_client')
 -- winbar requires NeoVim nightly, aka 0.9.x
 -- require('user.plugins.winbar')
-
