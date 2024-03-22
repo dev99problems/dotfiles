b4635881149855002ccbbef5f88d9ad92092510e
Gene Chulkov <genechulkov@gmail.com>
2024-03-22 18:40:17 +0000

    [nvim] adjust BufferLine config to it's final version — no major things left to configure

--- nvim/lua/user/plugins_configs/bufferline.lua
+++ nvim/lua/user/plugins_configs/bufferline.lua
@@ -1,8 +1,7 @@
 local utils = require('user.utils')
 local bufferline = utils.safe_require('bufferline')
-
--- NOTE: please do the bufferline switching work in order,
--- no matter if they were sorted to moved!!!
+-- :h bufferline-configuration
+-- OR https://github.com/akinsho/bufferline.nvim/blob/main/doc/bufferline.txt
 
 local get_current_dir = function ()
   -- NOTE: maybe you should pick only 
@@ -11,20 +10,21 @@ local get_current_dir = function ()
   return 'Project Tree'
 end
 
--- :h bufferline-configuration
--- OR https://github.com/akinsho/bufferline.nvim/blob/main/doc/bufferline.txt
 bufferline.setup {
   options = {
-    mode = 'buffers', -- 'tabs' | 'buffers'
+    mode = 'buffers',
     numbers = 'ordinal',
-    name_formatter = function()
-      -- name                | str        | the basename of the active file
+    -- name_formatter = function(buf)
+      -- name                | str        | the basename of the active file, like buf.name
       -- path                | str        | the full path of the active file
       -- bufnr (buffer only) | int        | the number of the active buffer
       -- buffers (tabs only) | table(int) | the numbers of the buffers in the tab
       -- tabnr (tabs only)   | int        | the "handle" of the tab, can be converted to its ordinal number using: `vim.api.nvim_tabpage_get_number(buf.tabnr)`
-    end,
-    -- max_name_length = 18,
+    -- end,
+    -- indicator = {
+    --   style = 'underline'
+    -- },
+    max_name_length = 20,
     -- max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
     truncate_names = true, -- whether or not tab names should be truncated
     -- tab_size = 21,
@@ -38,12 +38,12 @@ bufferline.setup {
         separator = true
       }
     },
-    -- color_icons = true | false, -- whether or not to add the filetype icon highlights
+    color_icons = true, -- whether or not to add the filetype icon highlights
     -- show_buffer_icons = true | false, -- disable filetype icons for buffers
-    -- show_buffer_close_icons = true | false,
+    show_buffer_close_icons = false,
     -- show_buffer_default_icon = true | false, -- whether or not an unrecognised filetype should show a default icon
     -- show_close_icon = true | false,
-    -- show_tab_indicators = true | false,
+    show_tab_indicators = true,
     -- show_duplicate_prefix = true | false, -- whether to show duplicate buffer prefix
     persist_buffer_sort = false, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
