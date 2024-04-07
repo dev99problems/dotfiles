d6712a65cae4dabd51db8aeae43d1e173ca99f13
Gene Chulkov <genechulkov@gmail.com>
2024-04-07 12:50:00 +0100

    [nvim] neoclip: remove empty lines from history

--- nvim/lua/user/plugins_configs/neoclip.lua
+++ nvim/lua/user/plugins_configs/neoclip.lua
@@ -1,9 +1,25 @@
 local neoclip = require('neoclip')
 
+local function is_whitespace(line)
+  return vim.fn.match(line, [[^\s*$]]) ~= -1
+end
+
+local function all(tbl, check)
+  for _, entry in ipairs(tbl) do
+    if not check(entry) then
+      return false
+    end
+  end
+  return true
+end
+
 neoclip.setup({
   enable_persistent_history = true,
   -- to make persistent history work, you need sqlite.lua plugin
   db_path = vim.fn.stdpath('data') .. '/databases/neoclip.sqlite3',
+  filter = function(data)
+    return not all(data.event.regcontents, is_whitespace)
+  end,
   keys = {
     telescope = {
       i = {
