91add254f8721ea806504f7f24042e031fdf5f25
Gene Chulkov <genechulkov@gmail.com>
2024-04-07 12:40:58 +0100

    [nvim] add properly looking icons for all nvim plugins

--- nvim/lua/user/plugins_configs/telescope.lua
+++ nvim/lua/user/plugins_configs/telescope.lua
@@ -7,7 +7,7 @@ local fb_actions = telescope.extensions.file_browser.actions
 telescope.setup {
   defaults = {
     prompt_prefix = ' ',
-    selection_caret = ' ',
+    selection_caret = '>',
     -- path_display = { 'smart' },
     layout_config = {
       horizontal = {
