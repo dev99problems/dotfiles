64846e6c05a212e40d5635fe0866cf35fc2c1789
Gene Chulkov <genechulkov@gmail.com>
2025-04-14 11:32:20 +0100

    [nvim] extend js snippets to astro

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -17,6 +17,7 @@ luasnip.filetype_extend('javascript', { 'html' })
 luasnip.filetype_extend('javascriptreact', { 'html' })
 luasnip.filetype_extend('typescript', { 'html' })
 luasnip.filetype_extend('typescriptreact', { 'html' })
+luasnip.filetype_extend('astro', { 'html', 'javascript' })
 
 local check_backspace = function()
   local col = vim.fn.col "." - 1
